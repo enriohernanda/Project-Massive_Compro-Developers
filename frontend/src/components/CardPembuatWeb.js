8b8ff71af7e8361f2527ed6b15c466a9ea878ce7
Enrio <enriohernanda@gmail.com>
2024-05-29 01:37:30 +0700

    update card pembuat web

--- frontend/src/components/CardPembuatWeb.js
+++ frontend/src/components/CardPembuatWeb.js
@@ -1,16 +1,10 @@
-import { Card } from 'react-bootstrap';
-
 const CardPembuatWeb = (props) => {
   return (
-    <Card style={{ width: '10rem' }}>
-      <Card.Img variant="top" src={props.image} />
-      <Card.Body>
-        <Card.Title>
-          <strong>{props.nama}</strong>
-        </Card.Title>
-        <Card.Text>{props.sebagai}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className="cardPembuat">
+      <img className="cardImgPembuat" src={props.image} alt="pembuat website" />
+      <h2 className="namaPembuat">{props.nama}</h2>
+      <p className="sebagaiPembuat">{props.sebagai}</p>
+    </div>
   );
 };
 
