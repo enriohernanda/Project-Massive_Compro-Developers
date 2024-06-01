472f5cbe7f1c7167d003ef6d69ea0ae03a222de4
ferrypradana07 <ferrykun500@gmail.com>
2024-06-01 21:28:36 +0700

    perbaikan authcontect

--- frontend/src/contex/AuthContext.js
+++ frontend/src/contex/AuthContext.js
@@ -1,24 +1,54 @@
 import React, { createContext, useEffect, useState} from "react";
 import axios from "axios";
-import { useHistory } from 'react-router-dom' 
-
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 export const AuthContext = createContext()
 
 export const AuthProvider = ({children}) => {
-    const [isauth, setAuth] = useState(false)
-    const [token, setToken] = useState()
-    const history = useHistory()
+    const [isAuth, setAuth] = useState(false)
+    const [token, setToken] = useState('')
+    const [userid, setUserId] = useState('')
+    const [username, setUsername] = useState('')
+    const [photo_profile, setUserphoto_profile] = useState('')
+    const navigate = useNavigate()
 
-    const login = async (email, password) => {
+    const registration = async (username, email, password) => {
         try {
-            const response = await axios.get('localhost:3000/api/authorization', { email, password })
+            const response = await axios.post('http://localhost:4000/api/user/registration', {username : username, email : email, password : password })
             console.log(response)
             if (response.data.token) {
                 setAuth(true)
                 setToken(response.data.token)
-                localStorage.setItem('isauth', isauth)
-                localStorage.setItem('token', token)
-                history.push("/")
+                setUserId(response.data.userid)
+                setUsername(response.data.username)
+                setUserphoto_profile(response.data.photoprofile)
+                localStorage.setItem('isauth', 'true')
+                localStorage.setItem('token', response.data.token)
+                localStorage.setItem('userid', response.data.userid)
+                localStorage.setItem('username', response.data.username)
+                navigate("/")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const login = async (email, password) => {
+        try {
+            const response = await axios.post('http://localhost:4000/api/user/authentication', {email : email,password :password })
+            console.log(response)
+            if (response.data.token) {
+                setAuth(true)
+                setToken(response.data.token)
+                setUserId(response.data.userid)
+                setUsername(response.data.username)
+                setUserphoto_profile(response.data.photoprofile)
+                localStorage.setItem('isauth', 'true')
+                localStorage.setItem('token', response.data.token)
+                localStorage.setItem('userid', response.data.userid)
+                localStorage.setItem('username', response.data.username)
+                localStorage.setItem('photoprofile', response.data.photoprofile)
+                navigate("/")
             }
         } catch (error) {
             console.log(error)
@@ -27,15 +57,39 @@ export const AuthProvider = ({children}) => {
     
    const logout = async () => {
     try {
-        const response = await axios.delete('localhost:4000/api/user/unauthorization', {token}) 
+        const token = localStorage.getItem('token')
+        const response = await axios.delete('http://localhost:4000/api/user/unauthorization', { params : {token : token} }) 
         if (response.data.status === 'success') {
-                setAuth(false)
-                setToken('')
-                localStorage.removeItem('isauth')
-                localStorage.removeItem('token')
+            setAuth(false)
+            setToken('')
+            setUserId()
+            setUsername('')
+            setUserphoto_profile('')
+            localStorage.removeItem('isauth')
+            localStorage.removeItem('token')
+            localStorage.removeItem('userid')
+            localStorage.removeItem('username')
+            localStorage.removeItem('photoprofile')
         }
     } catch (error) {
         console.log(error)
     }
    }
+useEffect (() => {
+    const authstatus = localStorage.getItem('isauth') === "true"
+    const tokendata = localStorage.getItem('token')
+    const urldata = localStorage.getItem('photoprofile')
+    const useriddata = localStorage.getItem('userid')
+    if (authstatus && tokendata && urldata && useriddata) {
+        setAuth(authstatus)
+        setToken(tokendata)
+        setUserId(useriddata)
+        setUserphoto_profile(urldata)
+    }
+},[])
+   return (
+       <AuthContext.Provider value={{isAuth, token, username, userid, photo_profile, registration, login, logout}} >
+            {children}
+       </AuthContext.Provider>
+   )
 }
